25e17d55ba12f23f43ff2c7b4f8cbf06d9e25535
Patrick Danaher <pdanaher@nanostring.com>
2024-04-19 09:37:23 -0700

    more explanation in vignette

--- code/FOV QC/FOV QC vignette.R
+++ code/FOV QC/FOV QC vignette.R
@@ -18,6 +18,11 @@ str(counts)
 counts[1:5, 1:5]
 head(xy)
 head(fov)
+# (Note : the above data objects can easily be extracted from a Giotto or Seurat or tileDB object. 
+# See the docs for whichever system you're using for how to extract count matrices and metadata columns.
+# Note that this code expects a count matrix with cells in rows & genes in columns, and 
+# some toolkits store a transposed version of this matrix. In this case, use Matrix::t() to transpose
+# your sparse counts matrix.)
 
 
 #### run QC pipeline -------------------
@@ -27,19 +32,28 @@ str(res)
 res$flaggedfovs
 # which genes are involved in the flagged bits in those FOVs:
 head(res$flagged_fov_x_gene)
+# list all the flagged genes (losing just one reporter cycle impacts a lot of genes):
+res$flagged_fov_x_gene[, "gene"]
 
 #### Explore results: -----------------
 
 # heatmap of estimated bias suffered for each FOV * bit (only flagged FOV * bits colored):
 FOVEffectsHeatmap(res) 
+# We see 2 bits (colors) flagged from 1 reporter cycle in 1 FOV, and 1 bit flagged in another FOV.
+# Biological variability can cause single bits to be flagged, so we only flag FOVs where >=2
+# bits from a single reporter cycle are flagged.
 
 ## spatial plots of per-bit FOV effects:
 par(mar = c(1,1,3,1))
 # show all bits from flagged reporter cycles (only reporter cycles with >= 2 flagged bits in a single FOV get shown):
+# Here we divide the tissue into sub-FOV grids, and we color each grid square by its change
+# in a barcode bit's total gene expression from similar grid squares from other FOVs.
 par(mfrow = c(2,2))
 FOVEffectsSpatialPlots(res = res, outdir = NULL, bits = "flagged_reportercycles") 
+# it's clear that all 4 bits from this reporter cycle are lost in the impacted FOV.
 
 # show all bits that were flagged:
 par(mfrow = c(2,2))
 FOVEffectsSpatialPlots(res = res, outdir = NULL, bits = "flagged_bits") 
-# we can see that one bit (reportercycle18R) was flagged in a different FOV, but it appears to be biology-driven, not artifactual
+# we can see that one bit (reportercycle18R) was flagged in a different FOV, but 
+#  it appears to be biology-driven, not a FOV-level technical artifact
