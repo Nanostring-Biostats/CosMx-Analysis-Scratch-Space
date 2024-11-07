6d697c33b768383ccd486e0cc23d42dc491d2dad
Patrick Danaher <pdanaher@nanostring.com>
2024-11-07 07:03:36 -0800

    more build out

--- _code/cell-typing-advanced-strategies/getSubtypingGenes.R
+++ _code/cell-typing-advanced-strategies/getSubtypingGenes.R
@@ -4,8 +4,25 @@
 #' @param ref Reference matrix for the cell types in question
 #' @param xy Spatial coordinates of cells  
 #' @return A vector of gene name for use in supervised cell subtyping
-getSubtypingGenes <- function(mat, ref, xy) {
+getSubtypingGenes <- function(mat, ref, xy = NULL) {
   
+  ## checks
+  if (!is.null(rownames(ref))) {
+    stop("the reference profiles matrix (ref) needs row names")
+  }
+  if (nrow(xy) != nrow(mat)) {
+    stop("xy and mat have different nrow")
+  }
+  ## identify hvgs in the ref profiles:
+  keepgenes <- ______
+  
+  ## find genes passing the contamination filter:
+  if (!is.null(xy)) {
+    safegenes <- _______________
+    
+    keepgenes <- intersect(keepgenes, safegenes)
+  }
+  return(keepgenes)
 } 
 
 
@@ -20,7 +37,27 @@ getSubtypingGenes <- function(mat, ref, xy) {
 #' @return A vector of gene named for use in unsupervised subclustering
 getSubclusteringGenes <- function(mat, xy) {
   
+  ## checks
+  if (nrow(xy) != nrow(mat)) {
+    stop("xy and mat have different nrow")
+  }
+  
+  ## identify hvgs in the counts matrix:
+  
+  
+  ## find genes passing the contamination filter:
+  if (!is.null(xy)) {
+    safegenes <- _______________
+    
+    keepgenes <- intersect(keepgenes, safegenes)
+  }
+  return(keepgenes)
 } 
 
 
-#' 
+#' Find genes safe from major contamination from cell segmentation errors
+#' @param mat Expression matrix from just the cell type in question, cells in rows and genes in columns.
+#' @param xy Spatial coordinates of cells 
+findSafeGenes <- function(mat, xy) {
+  
+}
