af7d7932d8c5e7f2370f75d87dc4037605e53ef0
Dan McGuire <dan11mcguire@gmail.com>
2025-12-01 19:19:20 +0000

    Squashed '_code/HieraType/' changes from 38c0aa1..79bffeb
    79bffeb Merge pull request #9 from Nanostring-Biostats/plotting_imports
    aa955b9 bump version
    1c9c261 ggplot2 function imports so package need not be explicitly imported
    
    git-subtree-dir: _code/HieraType
    git-subtree-split: 79bffebb6e118b9a926682cef227820fa945f0cf

--- R/imports.R
+++ R/imports.R
@@ -1,2 +1,3 @@
 #' @importFrom data.table .N .I ':=' copy uniqueN .SD
+#' @importFrom ggplot2 ggplot aes geom_point theme theme_classic scale_fill_gradientn element_text element_blank theme_bw scale_color_manual guide_legend scale_size_continuous
 NULL
